c8ee9f94bdef0dfd034df296fd64d8f2bdd9721a
Paul Lin <Paul.Lin@arris.com>
2017-02-01 14:19:18 -0500

    inited

---
+++ hello_ls.js
@@ -0,0 +1,21 @@
+
+// http://nodejs.org/api.html#_child_processes
+
+var sys = require('util')
+var exec = require('child_process').exec;
+var child;
+
+// executes `pwd`
+child = exec("pwd", function (error, stdout, stderr) {
+	console.log('stdout: ' + stdout);
+	console.log('stderr: ' + stderr);
+	if (error !== null) {
+		console.log('exec error: ' + error);
+	}
+});
+
+// or more concisely
+var sys = require('util')
+var exec = require('child_process').exec;
+function puts(error, stdout, stderr) { sys.puts(stdout) }
+exec("ls -la", puts);
